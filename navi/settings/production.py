d50dbbc7a01564216d673d8730a3f199f85c4264
droza0 <droza0@users.noreply.github.com>
2019-11-02 19:32:55 -0400

    fixed

--- navi/settings/production.py
+++ navi/settings/production.py
@@ -5,12 +5,11 @@ DEBUG = False
 DATABASES = {
     'default': {
                 'ENGINE': 'django.db.backends.postgresql',
-                'NAME': 'adapto_qu66',
-                'USER': 'adapto',
-                'PASSWORD': 'L8f5UOnL5t',
-                'HOST': 'dpg-blq3la4o8crha3ep3590',
+                'NAME': 'navi',
+                'USER': 'navi',
+                'PASSWORD': 'Vsdo7MUtZVohBx6dximgN5NdXUisGt6n',
+                'HOST': 'dpg-bmv0tsvd1hnnvsqk69o0',
                 'PORT': '5432',
     }
 }
 
-SECRET_KEY = 'w2p!z93l004jbhl_$-k0_q^znvqo3acvkbffth+!ah)96xd@h4'
